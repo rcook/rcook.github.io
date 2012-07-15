98a1a5371e1b2027844a52259ffd7cf5872c8320
Richard Cook <rcook@rcook.org>
2012-07-15 08:19:32 -0700

    Added more debugging code

--- app/dispatcher.php
+++ app/dispatcher.php
@@ -1,8 +1,27 @@
 <?php
 
+define('DEBUG_MODE', true);
+
 set_error_handler(function($severity, $message, $fileName, $lineNumber) {
   throw new ErrorException($message, 0, $severity, $fileName, $lineNumber);
 });
+
+if (DEBUG_MODE) {
+  register_shutdown_function(function() {
+    $errorInfo = error_get_last();
+    if (!is_null($errorInfo)) {
+      echo '<h1>Fatal error</h1><code><pre>';
+      print_r($errorInfo);
+      echo '</pre></code></body></html>';
+    }
+  });
+
+  set_exception_handler(function($exception) {
+    echo '<h1>Unhandled exception</h1><code><pre>';
+    print_r($exception);
+    echo '</pre></code>';
+  });
+}
 
 require_once realpath(__DIR__ . '/../classes/ClassAutoloader.class.php');
 new ClassAutoloader(array('classes'));

f60f0e2b882d8bb575914440c2d134948a93eb94
Richard Cook <rcook@rcook.org>
2012-07-16 09:36:58 -0700

    Split out parameters into separate module

--- templates/details.template.php
+++ templates/details.template.php
@@ -38,6 +38,7 @@ $bodyContent = getSection($content, 'body');
   <link rel="stylesheet" href="/static/style/default.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script src="/static/script/kblib.contents.js"></script>
+  <script src="/static/script/kblib.parameters.js"></script>
   <?= $headContent ?>
 </head>
 <body>

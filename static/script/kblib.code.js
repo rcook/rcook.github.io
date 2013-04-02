810232e1c2e62fbe294b47229f8aed789d7e93d2
Richard Cook <rcook@rcook.org>
2013-04-01 20:04:12 -0700

    Added kblib.code.js

---
+++ static/script/kblib.code.js
@@ -0,0 +1,21 @@
+$(function () {
+  var trimWhitespace = function (str) {
+    return str.replace(/^\s\s*/, "").replace(/\s\s*$/, "");
+  };
+
+  $(".code-block").each(function (index, item) {
+    var $item = $(item);
+    var lines = trimWhitespace($item.html()).split("\n");
+    var html = "";
+    $.each(lines, function (index, item) {
+      html += "<span class=\"prompt\">&gt;</span> " + item + "\n";
+    });
+    $item.html(html);
+  });
+
+  $(".content").each(function (index, item) {
+    var $item = $(item);
+    $item.html(trimWhitespace($item.html()));
+  });
+});
+

4b1a8489705378587afe0fcc979e2f825c6eef61
Richard Cook <rcook@rcook.org>
2013-04-03 12:55:34 -0700

    Dynamically generate parameters table

--- static/script/kblib.parameters.js
+++ static/script/kblib.parameters.js
@@ -61,35 +61,51 @@
       }
     };
 
-    populateParametersFromQueryString();
+    if (typeof Parameters !== "undefined") {
+      var $table = $("<table id=\"parameters\">");
+      $("#parameters-container").append($table);
+      $.each(Parameters, function (index, parameter) {
+        var name = parameter.name;
+        var fullName = parameter.fullName;
+        var defaultValue = parameter.defaultValue;
+        var html = "<tr>";
+        html += "<td><label for=\"" + name + "\">" + fullName + "</label></td>";
+        html += "<td><input data-kb-role=\"value\" id=\"" + name + "\" name=\"" + name + "\" type=\"text\" value=\"" + defaultValue + "\"></td>";
+        html += "</tr>";
+        $table.append(html);
+      });
+      $table.append("<tr><td colspan=\"2\"><button id=\"reset\">Reset</button><button id=\"link\">Link</button></td></tr.");
 
-    $("input[data-kb-role=value]").each(function () {
-      var $input = $(this);
-      $input.data("original-value", $input.val());
-      doValueUpdate($input);
-    }).keyup(function () {
-      var $input = $(this);
-      doValueUpdate($input);
-      updateControls();
-    });
+      populateParametersFromQueryString();
 
-    $("#reset").click(function () {
       $("input[data-kb-role=value]").each(function () {
         var $input = $(this);
-        $input.val($input.data("original-value"));
+        $input.data("original-value", $input.val());
+        doValueUpdate($input);
+      }).keyup(function () {
+        var $input = $(this);
         doValueUpdate($input);
         updateControls();
       });
-    });
 
-    $("#link").click(function () {
-      var link = createCustomizedLink();
-      $("<div class=\"kb-dialog\"><p>Copy and paste the following link and e-mail it to your friends:</p><p><a href=\"" + link + "\">" + link + "</a></p></div>").dialog({
-        title: "Customized link",
-        width: 500
+      $("#reset").click(function () {
+        $("input[data-kb-role=value]").each(function () {
+          var $input = $(this);
+          $input.val($input.data("original-value"));
+          doValueUpdate($input);
+          updateControls();
+        });
       });
-    });
 
-    updateControls();
+      $("#link").click(function () {
+        var link = createCustomizedLink();
+        $("<div class=\"kb-dialog\"><p>Copy and paste the following link and e-mail it to your friends:</p><p><a href=\"" + link + "\">" + link + "</a></p></div>").dialog({
+          title: "Customized link",
+          width: 500
+        });
+      });
+
+      updateControls();
+    }
   });
 

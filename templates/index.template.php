183ae25e061d8c79c24cf89c2364fccd1c0f595e
Richard Cook <rcook@rcook.org>
2013-04-04 09:15:35 -0700

    Fixed styling of index page

--- templates/index.template.php
+++ templates/index.template.php
@@ -9,11 +9,15 @@ $articles = $collection->getArticles();
 <head>
   <meta charset="utf-8">
   <title>Richard's KnowledgeBase</title>
+  <link rel="stylesheet" href="/static/style/default.css">
 </head>
 <body>
-<h1>Richard's KnowledgeBase</h1>
+<div class="banner">
+Richard's KnowledgeBase
+</div>
 <?
 if (count($articles) > 0) {
+  echo '<h1>Index of articles</h1>';
   echo '<ul>';
   foreach ($articles as $article) {
     echo '<li><a href="/' . sprintf('kb%03d', $article->getId()) . '/">' . sprintf('KB%03d', $article->getId()) . '&mdash;' . htmlspecialchars($article->getTitle()) . '</a></li>';
